3dadcef34e4997df2089e7990adc2ea248e5b420
forexpro <roma.minihanowa@gmail.com>
2024-01-22 20:24:27 +0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <v-app>
+    <v-container>
+      <v-btn style="width: 20px;">hello</v-btn>
+      <p>
+        Введение
+        Что такое SEO и почему оно важно
+        Как работает SEO
+        Основные принципы оптимизации сайта для поисковых систем
+        Ключевые факторы, влияющие на ранжирование сайта
+        Как создать контент, оптимизированный для SEO
+        Использование внутренних и внешних ссылок
+        Роль социальных медиа в SEO
+        Влияние мобильной оптимизации на SEO
+        Заключение
+      </p>
+      <div>rokirovochanet@mail.su</div>
+      <a href = "www.google.con">rokirovochanet@mail.su</a>
+    </v-container>
+
+    <!-- <RouterView /> -->
+  </v-app>
+</template>
+
+<style scoped></style>
